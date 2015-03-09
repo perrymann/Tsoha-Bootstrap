cd097b835c0f905fc1d658831d2e80d0d85c64bf
perrymann <perttu.nikander@cs.helsinki.fi>
2015-03-09 20:55:06 +0200

    Sovellus alustettu ja versionhallinta otettu käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -1,5 +1,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME=pnikande
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER=Tietokantasovellus
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
